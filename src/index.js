7b639f1e14650af6a112ea04b6beae4aa5178f29
BARANEESWARAN <baranees1@gmail.com>
2024-02-04 00:39:35 -0800

    clear unwanted codes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 
