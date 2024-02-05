6e15bcb57573a4822ddb091166e2ab1541e3e8fb
BARANEESWARAN <baranees1@gmail.com>
2024-02-04 22:17:06 -0800

    selectors file added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Selectors from './components/Selectors/Selectors';
 
 function App() {
   return (
     <div className="App">
   
+  <Selectors/>
     </div>
   );
 }
---
+++ src/components/Selectors/Selectors.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Selectors.css"
+function Selectors() {
+  return (
+    <div className='selectors'>
+
+<div className='selectors__container'>
+<div id='my-id'>
+If you are unable to use a Node version manager, you can use a Node installer to install both Node.js and npm on your system.
+
+Node.js installer
+NodeSource installer
+If you use Linux, we recommend that you use a NodeSource installer.
+
+OS X or Windows Node installers
+If you're using OS X or Windows, use one of the installers from the Node.js download page. Be sure to install the version labeled LTS. Other versions have not yet been tested with npm.
+
+Linux or other operating systems Node installers
+If you're using Linux or another operating system, use one of the following installers:
+</div>
+
+<div className='selectors__links'>
+<a href='google'>Gooogle</a>
+<a href='google.com'>google.com</a>
+<a href='htttp//google'>htttp//google</a>
+<a href='htttps//google.com/'>htttps//google.com</a>
+</div>
+
+</div>
+
+    </div>
+  )
+}
+
+export default Selectors
